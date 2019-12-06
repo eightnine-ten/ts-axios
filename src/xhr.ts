9f9356a5719edb862851bac6741bdfc6eb6ce541
宁柏龙 <418847406@qq.com>
2019-12-06 18:05:09 +0800

    feat(最基础实现): 最基础的请求发起 和 接受返回

---
+++ src/xhr.ts
@@ -0,0 +1,11 @@
+import { AxiosRequestConfig } from './types'
+
+export default function xhr(config: AxiosRequestConfig) {
+  const { data = null, url, method = 'get' } = config
+
+  const requset = new XMLHttpRequest()
+
+  requset.open(method.toUpperCase(), url, true)
+
+  requset.send(data)
+}
